2821b85957853d0214a0cba6c9ba067107cfb694
Alexander Waya Nguku <alex.waya@gmail.com>
2022-11-14 15:45:55 +0300

    add login & logout

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
+from django.contrib.auth.views import LoginView
+#from .forms import RegisterForm, LoginForm
 
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 
 def home(request):
     return render(request, 'users/home.html', {})
@@ -11,6 +13,14 @@ class RegisterView(View):
     form_class = RegistrationForm
     initial = {'key': 'value'}
     template_name = 'users/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # will redirect to the home page if a user tries to access the register page while logged in
+        if request.user.is_authenticated:
+            return redirect(to='/')
+
+        # else process dispatch as it otherwise normally would
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial=self.initial)
@@ -25,12 +35,26 @@ class RegisterView(View):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
 
-            return redirect(to='/')
+            return redirect(to='login')
 
         return render(request, self.template_name, {'form': form})
 
 
+class CustomLoginView(LoginView):
+    form_class = LoginForm
 
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get('remember_me')
+
+        if not remember_me:
+            # set session expiry to 0 seconds. So it will automatically close the session after the browser is closed.
+            self.request.session.set_expiry(0)
+
+            # Set session as modified to force data updates/cookie to be saved.
+            self.request.session.modified = True
+
+        # else browser session will be as long as the session cookie time "SESSION_COOKIE_AGE" defined in settings.py
+        return super(CustomLoginView, self).form_valid(form)
 
 
 
