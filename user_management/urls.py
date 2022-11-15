273c087a534178bc1d2a5f5585e0144d0bef2e23
Alexander Waya Nguku <alex.waya@gmail.com>
2022-11-15 22:06:50 +0300

    update core urls

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+#from django.conf.urls import url
+from django.urls import re_path
 
 from django.contrib.auth import views as auth_views
 from users.views import CustomLoginView  
@@ -12,4 +14,7 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
                                            authentication_form=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    #url(r'^oauth/', include('social_django.urls', namespace='social')),
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
